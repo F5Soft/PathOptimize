8bec98d0c0d26c1aa506d8a99ed04b816d48d9c3
F5Soft <f5.soft.dev@gmail.com>
2020-04-26 23:20:28 +0800

    首页拆分，实现车辆配置页面逻辑与接口

--- app.py
+++ app.py
@@ -1,28 +1,43 @@
-import json
 from flask import *
-from .modules.algorithm import *
+
+from modules.algorithm import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     """主页"""
-    return render_template("index.html")
+    trucks = get_trucks()
+    return render_template("index.html", trucks_data=trucks)
 
 
-@app.route('/trucks')
+@app.route('/trucks', methods=['GET'])
 def get_trucks():
-    """配置卡车接口"""
-    if request.method == 'POST':
-        set_trucks(request.data)
+    trucks = [{"i": 0, "name": "蓝翔挖掘机", "nums": 12, "d_max": 3, "w_max": 9},
+              {"i": 1, "name": "顺丰快递车", "nums": 8, "d_max": 12, "w_max": 6}]
+    return render_template("trucks.html", trucks_data=trucks)
 
 
-@app.route('/network')
+@app.route('/network', methods=['GET'])
 def get_network():
     """配置网络接口"""
-    if request.method == 'POST':
-        set_network(request.data)
+    return render_template("network.html")
+
+
+@app.route('/trucks', methods=['POST'])
+def set_trucks():
+    """配置卡车接口"""
+    trucks_data = request.form.getlist('name[]')
+    print(trucks_data)
+    return "Hello"
+    # if request.method == 'POST':
+    #    set_trucks(request.data)
+
+
+@app.route('/network', methods=['POST'])
+def set_network():
+    pass
 
 
 if __name__ == '__main__':
