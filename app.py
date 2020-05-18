bd168f4a9a42f6e5101a782e7a475d4aac74a160
F5Soft <f5.soft.dev@gmail.com>
2020-05-18 20:06:16 +0800

    分离echarts内容至单独文件，实现网络配置页面预览

--- app.py
+++ app.py
@@ -8,32 +8,43 @@ from modules.entity import Truck
 
 app = Flask(__name__)
 
-trucks_data = [("物流车辆", 4, 50.0, 12.0)]  # 默认有1种车辆，距离上限50km，载重上限8t，共8辆
-nodes_data = [(0, {'name': '配送中心', 'demand': 0.0}), (1, {'name': '配送点1', 'demand': 2.0}),
-              (2, {'name': '配送点2', 'demand': 1.5}), (3, {'name': '配送点3', 'demand': 4.5}),
-              (4, {'name': '配送点4', 'demand': 3.0}), (5, {'name': '配送点5', 'demand': 1.5}),
-              (6, {'name': '配送点6', 'demand': 4.0}), (7, {'name': '配送点7', 'demand': 2.5}),
-              (8, {'name': '配送点8', 'demand': 3.0})]
-edges_data = [(0, 1, {'weight': 7.4}), (0, 3, {'weight': 5.3}), (0, 2, {'weight': 12.1}), (0, 4, {'weight': 6.6}),
-              (0, 5, {'weight': 8.2}), (0, 6, {'weight': 11.9}), (0, 7, {'weight': 11.2}), (0, 8, {'weight': 10.8}),
-              (1, 2, {'weight': 5.8}), (1, 3, {'weight': 9.0}), (1, 4, {'weight': 7.1}), (1, 5, {'weight': 11.4}),
-              (1, 6, {'weight': 12.8}), (1, 7, {'weight': 6.9}), (1, 8, {'weight': 10.7}), (2, 3, {'weight': 12.2}),
-              (2, 4, {'weight': 9.4}), (2, 5, {'weight': 10.1}), (2, 6, {'weight': 13.7}), (2, 7, {'weight': 2.8}),
-              (2, 8, {'weight': 9.0}), (3, 4, {'weight': 4.9}), (3, 5, {'weight': 4.1}), (3, 6, {'weight': 7.8}),
-              (3, 7, {'weight': 11.9}), (3, 8, {'weight': 8.5}), (4, 5, {'weight': 4.0}), (4, 6, {'weight': 6.0}),
-              (4, 7, {'weight': 6.6}), (4, 8, {'weight': 4.0}), (5, 6, {'weight': 3.9}), (5, 7, {'weight': 10.0}),
-              (5, 8, {'weight': 4.4}), (6, 7, {'weight': 10.8}), (6, 8, {'weight': 5.7}), (7, 8, {'weight': 5.5})]
-
-# 尝试打开数据文件，如果不存在，则使用上文的默认数据
-with open("data/trucks.dat", 'rb') as f:
+# 尝试打开车辆数据文件，如果不存在，则使用默认数据
+try:
+    f = open("data/trucks.dat", 'rb')
     trucks_data = pickle.load(f)
+    f.close()
     print("[NOTE] 使用已保存的车辆信息")
-with open("data/nodes.dat", 'rb') as f:
+except FileNotFoundError:
+    trucks_data = [("物流车辆", 4, 50.0, 12.0)]  # 默认有1种车辆，距离上限50km，载重上限8t，共8辆
+
+# 尝试打开顶点数据文件，如果不存在，则使用默认数据
+try:
+    f = open("data/nodes.dat", 'rb')
     nodes_data = pickle.load(f)
+    f.close()
     print("[NOTE] 使用已保存的顶点信息")
-with open("data/edges.dat", 'rb') as f:
+except FileNotFoundError:
+    nodes_data = [(0, {'name': '配送中心', 'demand': 0.0}), (1, {'name': '配送点1', 'demand': 2.0}),
+                  (2, {'name': '配送点2', 'demand': 1.5}), (3, {'name': '配送点3', 'demand': 4.5}),
+                  (4, {'name': '配送点4', 'demand': 3.0}), (5, {'name': '配送点5', 'demand': 1.5}),
+                  (6, {'name': '配送点6', 'demand': 4.0}), (7, {'name': '配送点7', 'demand': 2.5}),
+                  (8, {'name': '配送点8', 'demand': 3.0})]
+
+# 尝试打开边数据文件，如果不存在，则使用默认数据
+try:
+    f = open("data/edges.dat", 'rb')
     edges_data = pickle.load(f)
-    print("[NOTE] 使用已保存的边信息")
+    f.close()
+except FileNotFoundError:
+    edges_data = [(0, 1, {'weight': 7.4}), (0, 3, {'weight': 5.3}), (0, 2, {'weight': 12.1}), (0, 4, {'weight': 6.6}),
+                  (0, 5, {'weight': 8.2}), (0, 6, {'weight': 11.9}), (0, 7, {'weight': 11.2}), (0, 8, {'weight': 10.8}),
+                  (1, 2, {'weight': 5.8}), (1, 3, {'weight': 9.0}), (1, 4, {'weight': 7.1}), (1, 5, {'weight': 11.4}),
+                  (1, 6, {'weight': 12.8}), (1, 7, {'weight': 6.9}), (1, 8, {'weight': 10.7}), (2, 3, {'weight': 12.2}),
+                  (2, 4, {'weight': 9.4}), (2, 5, {'weight': 10.1}), (2, 6, {'weight': 13.7}), (2, 7, {'weight': 2.8}),
+                  (2, 8, {'weight': 9.0}), (3, 4, {'weight': 4.9}), (3, 5, {'weight': 4.1}), (3, 6, {'weight': 7.8}),
+                  (3, 7, {'weight': 11.9}), (3, 8, {'weight': 8.5}), (4, 5, {'weight': 4.0}), (4, 6, {'weight': 6.0}),
+                  (4, 7, {'weight': 6.6}), (4, 8, {'weight': 4.0}), (5, 6, {'weight': 3.9}), (5, 7, {'weight': 10.0}),
+                  (5, 8, {'weight': 4.4}), (6, 7, {'weight': 10.8}), (6, 8, {'weight': 5.7}), (7, 8, {'weight': 5.5})]
 
 
 @app.route('/', methods=['GET'])
@@ -65,7 +76,20 @@ def get_network():
     获取网络配置页面
     :return: 渲染后的网络配置页面
     """
+    Network(nodes_data, edges_data, []).gexf_summary("network_raw.gexf")
     return render_template("network.html", nodes_data=nodes_data, edges_data=edges_data)
+
+
+@app.route('/gexf', methods=['GET'])
+def get_gexf():
+    """
+    获取配送网络可视化信息
+    :return: 配送网络的gexf文件内容
+    """
+    if request.args.get('raw', None):
+        return render_template("gexf/network_raw.gexf")
+    else:
+        return render_template("gexf/network.gexf")
 
 
 @app.route('/trucks', methods=['POST'])
@@ -137,26 +161,18 @@ def apply_algorithm():
     trucks_initial = []
     for t in trucks_data:
         for i in range(int(t[1])):
-            trucks_initial.append(Truck(t[0], float(t[2]), float(t[3])))
+            trucks_initial.append(Truck(t[0] + str(i + 1), float(t[2]), float(t[3])))
     # 生成初始网络。网络数据来自第一个测试数据集
     network_initial = Network(nodes_data, edges_data, trucks_initial)
     # 初始化种群并开始遗传算法
     # todo 调整遗传算法参数：种群数，突变率等，让算法收敛的快些
-    population = alg.population_init(network_initial, 10)
-    network_best = alg.genetic_algorithm(population, mutation_rate=0.1, recombination_rate=0.6)
+    population = alg.population_init(network_initial, 20)
+    network_best = alg.genetic_algorithm(population, mutation_rate=0.15, recombination_rate=0.7)
     print("[NOTE] 算法输出结果:")
     print(network_best)
+    print("[NOTE] 适应度函数值:", network_best.adaptive())
     network_best.gexf_summary("network.gexf")
     return str(network_best)
-
-
-@app.route('/gexf', methods=['GET'])
-def get_gexf():
-    """
-    获取配送网络可视化信息
-    :return: 配送网络的gexf文件内容
-    """
-    return render_template("gexf/network.gexf")
 
 
 if __name__ == '__main__':
